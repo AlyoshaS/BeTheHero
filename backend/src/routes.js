ec4e36cb692d88c9424c2c5f7039e3759b2189e3
Evelyn Schmitz <evelynschmitz1@gmail.com>
2020-03-29 19:04:10 -0300

    feat: added SessionController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,19 +2,20 @@ const express = require('express')
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
-
-const connection = require('./database/connection')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router();
 
-routes.get('/ongs', OngController.index)
+routes.post('/sessions', SessionController.create);
 
+routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.get('/profile', ProfileController.index);
+
 routes.post('/incidents', IncidentController.create);
-
 routes.get('/incidents', IncidentController.index);
-
 routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
