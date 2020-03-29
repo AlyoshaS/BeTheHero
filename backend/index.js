bba58021cd3bf22238764396e80c8a78acff1c25
Evelyn Schmitz <evelynschmitz1@gmail.com>
2020-03-29 16:34:51 -0300

    chore: updated DB infos.

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,32 @@ app.use(express.json())
  */
 
 /**
-* Query Params: Parâmetros nomeados enviados na roda após "?"(Filtros, paginação)
-*/
+ * Exemplos db:
+ * SQL: MySQL, SQLite, PostgreeSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CounchDB, etc..
+ * 
+ * O MongoDB é uma ótima pedida pra quem tá iniciando os estudos em node principalmente por
+ * ser mais fácil de trabalhar que com bancos relacionais, claro que para coisas mais simples.
+ * Para coisas mais complexas ele acaba se tornando, na parte de relacionamentos, um pouco mais
+ * complicado de se aprender.
+ * 
+ * A decisão tomada foi de usar SQLite para se aproximar mais do que é usado no mercado e pelas questões
+ * citadas acima em relação ao mongoDB.
+ * 
+ * PS: as queryes podem ter diferenças dependendo do banco utilizado.
+ * 
+ * Configuração do DB:
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ * O Query builder selecionado foi o KNEX.JS http://knexjs.org/
+ * O Driver sera o sqlite3 porque vamos usar o SQLite.
+ * 
+ * Para inicializar o knex: npx knex init
+ */
+
+/**
+ * Query Params: Parâmetros nomeados enviados na roda após "?"(Filtros, paginação)
+ */
 app.get('/users', (request, response) => {
   return response.json({
     evento: 'Semana OmniStack 11.0',
