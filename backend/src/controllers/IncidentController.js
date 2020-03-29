cde87adc900ca4ce334284a94ff2aa0773d96a51
Evelyn Schmitz <evelynschmitz1@gmail.com>
2020-03-29 19:14:06 -0300

    feat: added pagination to incidentController

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,8 +2,25 @@ const connection = require('../database/connection');
 
 module.exports = {
   async index(request, response) {
-    const incidents = await connection('incidents').select('*')
+    const { page = 1 } = request.query;
 
+    const [count] = await connection('incidents').count();
+
+    // paginação
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        'incidents.*',
+        'ongs.name', 
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.uf'
+      ])
+
+    response.header('X-Total-Count', count['count(*)'])
     return response.json(incidents);
   },
 
