e0b5d869e9a2acb8ca42316ee28e1ad60cd2dc6f
Evelyn Schmitz <evelynschmitz1@gmail.com>
2020-03-29 18:00:53 -0300

    feat: added route for create ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,24 @@
 const express = require('express')
+const crypto = require('crypto')
+const connection = require('./database/connection')
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-  const body = request.body;
+routes.post('/ongs', async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
 
-  console.log(body);
+  const id = crypto.randomBytes(4).toString('HEX');
 
-  return response.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Evelyn Schmitz'
-  });
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  })
+
+  return response.json({ id });
 });
 
 module.exports = routes;
